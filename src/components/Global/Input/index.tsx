be9ec9c26ea59abc42101ccabe40a7e1805eb0c6
yuqi88 <snow34545@gmail.com>
2021-10-14 02:58:57 -0700

    Added input component and styling

---
+++ src/components/Global/Input/index.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+type IndexProps = {
+    type: 'text' | 'password',
+    id: string,
+    required: boolean,
+    placeholder?: string,
+    maxLength?: number,
+    labelText?: string,
+};
+const Index = ({type, id, required, placeholder, maxLength, labelText}: IndexProps) => {
+    const [input, setInput] = useState('');
+    return (
+        <>
+            <label htmlFor={id}>{labelText}</label>
+            <input
+                type={type}
+                id={id}
+                required={required}
+                placeholder={placeholder}
+                maxLength={maxLength}
+                value={input}
+                onChange={event => setInput(event.target.value)}/>
+        </>
+    );
+};
+
+export default Index;
