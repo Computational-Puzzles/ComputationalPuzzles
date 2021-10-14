5a6fc9ed6e98516e8e92264e74ae46d0565c3a17
opeyem1a <opeyadeyemi@gmail.com>
2021-10-14 12:16:56 -0700

    + fixed visibility for scss module files (I think?) + configured jestjs for typescript

--- components/example/index.tsx
+++ components/example/index.tsx
@@ -1,9 +1,11 @@
-import styles from './example.module.scss'
+import styles from './example.module.scss';
 
-export default function Example() {
+const Example = () => {
     return (
         <h3 className={styles.redHeading}>
             Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h3>
     )
-}
+};
+
+export default Example;
--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,8 @@ module.exports = {
         https://jestjs.io/docs/webpack#handling-static-assets */
         '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
     },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    preset: 'ts-jest',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
     testEnvironment: 'jsdom',
     transform: {
