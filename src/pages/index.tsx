07bfd04d21178c7e0f32323e6dc340590d4b17f9
opeyem1a <opeyadeyemi@gmail.com>
2021-10-21 04:19:30 -0700

    + added center mixin (in layouts mixins file) + delete redundant /styles + finish button component + clean up button styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import Button from "../components/Globals/Button";
+import Button from '../components/Globals/Button';
+
+const func1 = () => {
+    alert("Howdy")
+}
+
+const func2 = () => {
+    alert("Howdy 2")
+}
+
+const func3 = () => {
+    alert("Howdy 3")
+}
 
 const Home: React.FC = () => {
   const { data: session, status } = useSession();
@@ -15,9 +27,16 @@ const Home: React.FC = () => {
   }
   return (
     <>
-        <Button type={'primary'} content={'Test Button'}/>
-        <Button type={'secondary'} content={'Test Button 2'} arrowDirection={'right'}/>
-        <Button type={'outline'} content={'Test Button 2'}/>
+      <div>
+        <Button type={'primary'} content={'Test Button'} arrowDirection={'right'} onClick={func1}/>
+        <Button
+          type={'secondary'}
+          content={'Test Button 2'}
+          arrowDirection={'down'}
+          onClick={func2}
+        />
+        <Button type={'outline'} content={'Test Button 2'} arrowDirection={'right'} onClick={func3}/>
+      </div>
       <h1>Not logged in</h1>
       <button onClick={() => signIn()}>Login</button>
     </>
