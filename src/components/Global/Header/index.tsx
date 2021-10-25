f5ea3088f0d774256911db78d808d82c284882aa
Kiet Phan <tuankietvn@outlook.com>
2021-10-24 20:07:35 -0700

    Resolve comments

--- src/components/Global/Header/index.tsx
+++ src/components/Global/Header/index.tsx
@@ -1,43 +1,50 @@
 import * as React from 'react';
-import Image from 'next/image'
-
-import logo from './logo.png';
 import styles from './Header.module.scss';
 
+import { Logo } from '../';
+
 type HeaderProps = {
-    profilePicture?: string;
-}
+  profilePicture?: string;
+};
 
 const Header = ({ profilePicture }: HeaderProps) => {
-    const [activeTab, setActiveTab] = React.useState<0 | 1>(0);
+  const [activeTab, setActiveTab] = React.useState<0 | 1>(0);
 
-    return (
-        <div className={ styles.header }>
-            <div className={ styles.content}>
-                <div className={ styles.left }>
-                    <>
-                        <Image src={ logo } width={57} height={57} /> { /** This is a mock, change when there's logo component  */ }
-                        <p>Computational Puzzles for Kids</p>
-                    </>
-                    <div className={ styles.tabs }>
-                        <span className={ activeTab === 0 && styles.active } onClick={() => setActiveTab(0)}>
-                            <p>Puzzles Map</p>
-                        </span>
-                        <span className={ activeTab === 1 && styles.active } onClick={() => setActiveTab(1)}>
-                            <p>Puzzles List</p>
-                        </span>
-                    </div>
-                </div>
-                <div className={ styles.right }>
-                    <button> Profile </button>
-                    {   profilePicture
-                        ? <img  className={ styles.profileImg } src={ profilePicture } alt="profile" />
-                        : <span className={ styles.profileImg }> </span>
-                    }
-                </div>
-            </div>
+  return (
+    <div className={styles.header}>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <Logo showMark={true} showType={true} />
+          <div className={styles.tabs}>
+            <button
+              className={activeTab === 0 && styles.active}
+              onClick={() => setActiveTab(0)}
+            >
+              <span>Puzzles Map</span>
+            </button>
+            <button
+              className={activeTab === 1 && styles.active}
+              onClick={() => setActiveTab(1)}
+            >
+              <span>Puzzles List</span>
+            </button>
+          </div>
         </div>
-    );
-}
+        <div className={styles.right}>
+          <button> Profile </button>
+          {profilePicture ? (
+            <img
+              className={styles.profileImg}
+              src={profilePicture}
+              alt="profile"
+            />
+          ) : (
+            <span className={styles.profileImg}> </span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
---
+++ src/pages/puzzles/[id].tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PuzzlePage = () => {
+  return <div>Puzzle Page</div>;
+};
+
+export default PuzzlePage;
