37132ab802acf4bafd4061692bfd5202f91a0ac0
yuqi88 <snow34545@gmail.com>
2021-10-27 16:20:18 -0700

    ran prettier; added condition to display labelText; changed style; combined imports

--- src/components/Global/Input/index.tsx
+++ src/components/Global/Input/index.tsx
@@ -1,30 +1,40 @@
-import React, {useState} from 'react';
-import styles from "./Input.module.scss";
+import React, { useState } from 'react';
+import styles from './Input.module.scss';
 
 type IndexProps = {
-    type: 'text' | 'password',
-    id: string,
-    required: boolean,
-    placeholder?: string,
-    maxLength?: number,
-    labelText?: string,
+  type: 'text' | 'password';
+  id: string;
+  required: boolean;
+  placeholder?: string;
+  maxLength?: number;
+  labelText?: string;
 };
-const Index = ({type, id, required, placeholder, maxLength, labelText}: IndexProps) => {
-    const [input, setInput] = useState('');
-    return (
-        <>
-            <label htmlFor={id}>{labelText}</label>
-            <input
-                className={styles.input}
-                type={type}
-                id={id}
-                required={required}
-                placeholder={placeholder}
-                maxLength={maxLength}
-                value={input}
-                onChange={event => setInput(event.target.value)}/>
-        </>
-    );
+const Index = ({
+  type,
+  id,
+  required,
+  placeholder,
+  maxLength,
+  labelText
+}: IndexProps) => {
+  const [input, setInput] = useState('');
+  return (
+    <>
+      {labelText !== null &&
+        <label htmlFor={id}>{labelText}</label>
+      }
+      <input
+        className={styles.input}
+        type={type}
+        id={id}
+        required={required}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        value={input}
+        onChange={event => setInput(event.target.value)}
+      />
+    </>
+  );
 };
 
 export default Index;
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,46 +1,45 @@
-import React from "react";
-import styles from "../../../styles/login.module.scss";
-import Input from "../../components/Global/Input";
-import Logo from "../../components/Global/Logo/Logo";
+import React from 'react';
+import styles from '../../../styles/login.module.scss';
+import { Input, Logo } from '../../components/Global';
 
+const Login = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    // check: username & password (need data frm db?)
+  };
 
-const Login: React.FC = () => {
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // check: username & password (need data frm db?)
-    }
-
-
-    return (
-        <div className={styles.loginPage}>
-            <Logo showMark={true} showType={true}/>
-            <h2>Log In</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <Input type={'text'}
-                           id={'username'}
-                           required={true}
-                           placeholder={'Username'}
-                    />
-                    <br/>
-                    <Input
-                        type={'password'}
-                        id={'password'}
-                        required={true}
-                        placeholder={'Password'}
-                    />
-                    <br/>
-                    <a href='#' className='left'>Forgot Password?</a>
-                    <a href='#' className='right'>Do not have an account?</a>
-                    <br/>
-                </div>
-                <button> Log In</button>
-            </form>
-
+  return (
+    <div className={styles.loginPage}>
+      <Logo showMark={true} showType={true} />
+      <h2>Log In</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <Input
+            type={'text'}
+            id={'username'}
+            required={true}
+            placeholder={'Username'}
+          />
+          <br />
+          <Input
+            type={'password'}
+            id={'password'}
+            required={true}
+            placeholder={'Password'}
+          />
+          <br />
+          <a href="#" className="left">
+            Forgot Password?
+          </a>
+          <a href="#" className="right">
+            Do not have an account?
+          </a>
+          <br />
         </div>
-
-    )
-}
+        <button> Log In</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
