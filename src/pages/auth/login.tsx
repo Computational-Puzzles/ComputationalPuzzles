b71c8f27a418e6f727ed6b318746f1e9b5c1d3b7
yuqi88 <snow34545@gmail.com>
2021-11-02 07:35:58 -0700

    change class names & add TODO

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -5,49 +5,44 @@ import { Input, Logo } from '../../components/Global';
 const Login = () => {
   const handleSubmit = e => {
     e.preventDefault();
-    // check: username & password (need data frm db?)
+    // TODO: check username & password
   };
 
   return (
-    <div >
+    <div>
       <div className={styles.logoLogin}>
         <Logo showMark={true} showType={true} />
       </div>
-      <div className={styles.box1}>
-        <h2 className={styles.box2}>Log In</h2>
-        <form onSubmit={handleSubmit} >
-          <div >
-            <div className={styles.box3}>
+      <div className={styles.mainSec}>
+        <h2 className={styles.title}>Log In</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <div className={styles.inputs}>
               <Input
-                  type={'text'}
-                  id={'username'}
-                  required={true}
-                  placeholder={'Username'}
+                type={'text'}
+                id={'username'}
+                required={true}
+                placeholder={'Username'}
               />
             </div>
-            <div className={styles.box3}>
+            <div className={styles.inputs}>
               <Input
-                  type={'password'}
-                  id={'password'}
-                  required={true}
-                  placeholder={'Password'}
+                type={'password'}
+                id={'password'}
+                required={true}
+                placeholder={'Password'}
               />
             </div>
             <div className={styles.container}>
               {/*TODO: delete this tag or link to a password recovery page*/}
-              <a href="#" >
-                Forgot Password?
-              </a>
+              <a href="#">Forgot Password?</a>
               {/*TODO: change the link to the sign up page*/}
-              <a href="#" >
-                Do not have an account?
-              </a>
+              <a href="#">Do not have an account?</a>
             </div>
           </div>
           <button className={styles.button}> Log In</button>
         </form>
       </div>
-
     </div>
   );
 };
