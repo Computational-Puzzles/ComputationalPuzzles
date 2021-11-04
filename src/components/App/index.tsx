f74454135263a5f9a7fa15180c13230978f755fe
MtheDV <mathewdevin03@gmail.com>
2021-11-04 12:23:33 -0700

    Move map renderer to app components

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,5 +2,6 @@ import CardGrid from './CardGrid';
 import CardList from './CardList';
 import QRGenerator from './QRGenerator';
 import PuzzleInput from './PuzzleInput';
+import MapRenderer from "./MapRenderer";
 
-export { CardGrid, CardList, QRGenerator, PuzzleInput };
+export { CardGrid, CardList, QRGenerator, PuzzleInput, MapRenderer };
--- src/pages/puzzles/map/index.tsx
+++ src/pages/puzzles/map/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapRenderer } from '../../../components/Global';
+import { MapRenderer } from '../../../components/App';
 
 const PuzzleMap = () => {
   return (
