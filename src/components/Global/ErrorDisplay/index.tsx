f85c3701a9e73033a62a63eed5b421202d8c4226
Kiet Phan <tuankietvn@outlook.com>
2021-11-19 00:39:41 -0800

    Prettify

--- src/components/Global/ErrorDisplay/index.tsx
+++ src/components/Global/ErrorDisplay/index.tsx
@@ -2,19 +2,25 @@ import * as React from 'react';
 import styles from './ErrorDisplay.module.scss';
 import { Logo } from '../';
 
-const ErrorDisplay = ({ errorCode, message }: { errorCode: number, message: string }) => {
+const ErrorDisplay = ({
+  errorCode,
+  message
+}: {
+  errorCode: number;
+  message: string;
+}) => {
   return (
-    <div className={ styles.center } >
-      <div className={ styles.contentWrapper }>
-        <Logo showMark={ true } showType={ true } />
-        <span className={ styles.seperator } />
-        <div className={ styles.textWrapper }>
-          <p className={ styles.errorCode }> Error code: { errorCode } </p>
-          <p className={ styles.msg }> { message } </p>
+    <div className={styles.center}>
+      <div className={styles.contentWrapper}>
+        <Logo showMark={true} showType={true} />
+        <span className={styles.seperator} />
+        <div className={styles.textWrapper}>
+          <p className={styles.errorCode}> Error code: {errorCode} </p>
+          <p className={styles.msg}> {message} </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default ErrorDisplay;
