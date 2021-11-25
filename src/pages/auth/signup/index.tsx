96ae509c72c2bc5754d6be7bca7e90bd6116cde8
yuqi88 <snow34545@gmail.com>
2021-11-25 00:56:30 -0800

    styling signup page

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,56 +1,56 @@
-import React, {useState} from 'react';
-import {useSession} from "next-auth/react";
-import Router from "next/router";
-import {signUp} from "../../../services";
-import {Logo} from "../../../components/Global";
+import React, { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import Router from 'next/router';
+import { signUp } from '../../../services';
+import { Logo,Input } from '../../../components/Global';
+import styles from "../../../styles/pages/signup.module.scss";
 
 const SignUpPage = () => {
-    const { data: session, status } = useSession();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState(''); //is this a bad idea?
-    const [repeatPw, setRepeatPw] = useState(''); //is this a bad idea?
-    const passwordMinLength = 8;
+  const { data: session, status } = useSession();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPw, setRepeatPw] = useState('');
+  const passwordMinLength = 8;
 
-    console.log('session ',  session);
-    const handleSignUp = (event) =>{
-        //condition1: if the user is already logged in, alert: you want to make a new account? if yes>, if no>
-        //condition2: if pw != repeated pw, retype plz
-        if(password !== repeatPw){
-            alert('password and repeated password do not match. please try retyping both.');
-            event.preventDefault();
-            return false;
-        }
-        if(status === 'authenticated') {
-            if(window.confirm('Would you like to make another account?'))
-                window.alert('Please log out before you make an other account.');
-            event.preventDefault();
-            return false;
-        } else{
-            signUp({email,password});
-            event.preventDefault();
-            Router.push('/auth/login');
-        }
-
+  console.log('session ', session);
+  const handleSignUp = event => {
+    if (password !== repeatPw) {
+      alert(
+        'password and repeated password do not match. please try retyping both.'
+      );
+      event.preventDefault();
+      return false;
     }
+    if (status === 'authenticated') {
+      if (window.confirm('Would you like to make another account?'))
+        window.alert('Please log out before you make an other account.');
+      event.preventDefault();
+      return false;
+    } else {
+      signUp({ email, password });
+      event.preventDefault();
+      Router.push('/auth/login');
+    }
+  };
   return (
-      <div>
-          <Logo showMark={true} showType={true}/>
-          <h1>Sign Up Page</h1>
-          <form onSubmit={ ()=>handleSignUp(event)}>
-              <input type={'email'} placeholder={'Email'} onChange={ (event)=> setEmail(event.target.value)}/>
-              <br/>
-              <input type={'password'} placeholder={'Password'} minLength={passwordMinLength} onChange={ (event)=> setPassword(event.target.value)}/>
-              <br/>
-              <input type={'password'} placeholder={'Repeat password'} minLength={passwordMinLength} onChange={ (event)=> setRepeatPw(event.target.value)}/>
-              <br/>
-              <button type='submit'>Sign Up</button>
-          </form>
-          <a href={'/auth/login'}>login page</a>
-
-
+    <>
+      <div className={styles.logoLogin}>
+        <Logo showMark={true} showType={true} />
       </div>
+      <div className={styles.mainSec}>
+        <h2 className={styles.title}>Sign Up Page</h2>
+        <form onSubmit={() => handleSignUp(event)}>
+          <div className={styles.inputContainer}>
+            <Input type={'email'} id={'email'} required={true} placeholder={'Email'} setInputVal={setEmail}/>
+            <Input type={'password'} id={'password'} required={true} minLength={passwordMinLength} placeholder={'password'} setInputVal={setPassword}/>
+            <Input type={'password'} id={'repeatPassword'} required={true} minLength={passwordMinLength} placeholder={'repeat password'} setInputVal={setRepeatPw}/>
+          </div>
+          <p className={styles.link}><a href={'/auth/login'} >Already have an account?</a></p>
+          <button type="submit" className={styles.button}>Sign Up</button>
+        </form>
+      </div>
+    </>
   );
-
 };
 
 export default SignUpPage;
