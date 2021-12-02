3224f84014d75f3a1bb2c7394929862c3386b09a
MtheDV <mathewdevin03@gmail.com>
2021-12-02 15:46:50 -0800

    Use new Input component for searching and prettify code

--- src/components/App/MapGeocoder/index.tsx
+++ src/components/App/MapGeocoder/index.tsx
@@ -1,41 +1,48 @@
-import React, {Dispatch, SetStateAction, useState} from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
 import { Geocoder } from '@maptiler/geocoder';
-import {Anchor} from "../MapRenderer";
+import { Anchor } from '../MapRenderer';
+import { Button, Input } from '../../Global';
+
+import mapGeocoderStyles from './MapGeocoder.module.scss';
 
 const MAPTILER_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAPTILER_ACCESS_TOKEN;
 
 type MapGeocoderProps = {
   setMapCenter: Dispatch<SetStateAction<Anchor>>;
-}
+};
 
 const MapGeocoder = ({ setMapCenter }: MapGeocoderProps) => {
   const [geocoderSearchValue, setGeocoderSearchValue] = useState<string>('');
-  const [geocoderGeocodedPlace, setGeocoderGeocodedPlace] = useState<string>('');
-  
+
   let geocoder = new Geocoder({
     key: MAPTILER_ACCESS_TOKEN
   });
-  
+
   const onGeocoderSearch = (): void => {
-    setGeocoderSearchValue(geocoderSearchValue);
-    geocoder.geocode(geocoderSearchValue).then(results => {
-      const response = results.features[0];
-      setGeocoderGeocodedPlace(`${response.place_name} ${response.center}`);
-      setMapCenter([response.center[1], response.center[0]]);
-    });
+    if (geocoderSearchValue !== '') {
+      geocoder.geocode(geocoderSearchValue).then(results => {
+        const response = results.features[0];
+        setMapCenter([response.center[1], response.center[0]]);
+      });
+    }
   };
 
   return (
-    <div>
-      <input
-        type="search"
-        value={geocoderSearchValue}
-        onChange={event => setGeocoderSearchValue(event.target.value)}
+    <div className={mapGeocoderStyles.mapGeocoder}>
+      <Input
+        type={'text'}
+        id={'mapInput'}
+        required={false}
+        setInputVal={setGeocoderSearchValue}
+        placeholder={'Search For A Location'}
       />
-      <button type="button" onClick={onGeocoderSearch}>
-        Search
-      </button>
-      {geocoderGeocodedPlace}
+      <Button
+        style={'primary'}
+        content={'Search'}
+        onClick={onGeocoderSearch}
+        arrowDirection={'right'}
+        size={'sm'}
+      />
     </div>
   );
 };
