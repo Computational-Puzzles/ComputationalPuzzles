7dfb69b1c4714c7f2332285bb151675a0149fdb1
Kiet Phan <tuankietvn@outlook.com>
2021-12-07 17:31:52 -0800

    Add 403 page and relocate according to the docs

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { ErrorDisplay } from '../../components/Global';
+import { ErrorDisplay } from '../components/Global';
 
 const Error404 = () => {
   return <ErrorDisplay errorCode={404} message="Are you lost ???" />;
