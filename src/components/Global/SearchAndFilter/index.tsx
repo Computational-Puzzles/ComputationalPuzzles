f6dbe74a6f17383687384c60f71f3abcdc7b96ce
yuqi88 <snow34545@gmail.com>
2021-12-14 11:53:20 -0800

    run prettify

--- src/components/Global/SearchAndFilter/index.tsx
+++ src/components/Global/SearchAndFilter/index.tsx
@@ -1,35 +1,46 @@
-import React, {useEffect, useState} from 'react';
-import {Filter, Input} from "../index";
-import styles from './SearchAndFilter.module.scss'
+import React, { useEffect, useState } from 'react';
+import { Filter, Input } from '../index';
+import styles from './SearchAndFilter.module.scss';
 
 type SearchAndFilterProps = {
-    setSearchNFilterVal?: React.Dispatch<React.SetStateAction<object>>;
-}
+  setSearchNFilterVal?: React.Dispatch<React.SetStateAction<object>>;
+};
 
-const SearchAndFilter = ({setSearchNFilterVal}: SearchAndFilterProps) => {
-    const [searchText, setSearchText] = useState('');
-    const [filterFields,setFilterFields] = useState({easy:false, medium:false, hard: false})
+const SearchAndFilter = ({ setSearchNFilterVal }: SearchAndFilterProps) => {
+  const [searchText, setSearchText] = useState('');
+  const [filterFields, setFilterFields] = useState({
+    easy: false,
+    medium: false,
+    hard: false
+  });
 
-    useEffect(()=>{
-        setSearchNFilterVal &&
-        setSearchNFilterVal( {
-            searchText: searchText,
-            filterFields: filterFields,
-        })
-    },[searchText, filterFields])
+  useEffect(() => {
+    setSearchNFilterVal &&
+      setSearchNFilterVal({
+        searchText: searchText,
+        filterFields: filterFields
+      });
+  }, [searchText, filterFields]);
 
-    return (
-        <>
-            <div className={styles.container}>
-                <div className={styles.input}>
-                    <Input type={'text'} id={'search'} required={false} labelText={'Puzzle List'} placeholder={'Search'} setInputVal={setSearchText}/>
-                </div>
-                <div>
-                    <Filter setFilterFields={setFilterFields} />
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.input}>
+          <Input
+            type={'text'}
+            id={'search'}
+            required={false}
+            labelText={'Puzzle List'}
+            placeholder={'Search'}
+            setInputVal={setSearchText}
+          />
+        </div>
+        <div>
+          <Filter setFilterFields={setFilterFields} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SearchAndFilter;
--- src/components/Global/index.tsx
+++ src/components/Global/index.tsx
@@ -6,6 +6,6 @@ import Input from './Input';
 import Header from './Header';
 import Card from './Card';
 import ErrorDisplay from './ErrorDisplay';
-import SearchAndFilter from "./SearchAndFilter";
+import SearchAndFilter from './SearchAndFilter';
 
 export { Button, Card, Header, Logo, Input, RadioButton, Filter, ErrorDisplay, SearchAndFilter};
