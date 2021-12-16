5720339cc4b844d3710fec87e85c6527252e9129
yuqi88 <snow34545@gmail.com>
2021-12-16 01:31:29 -0800

    add types and logic for search puzzle name

--- src/pages/puzzles/list/index.tsx
+++ src/pages/puzzles/list/index.tsx
@@ -1,67 +1,79 @@
-import React, {useState} from 'react';
-import {GetServerSideProps} from "next";
-import {getAllPuzzleInstances} from "../../../services/puzzleInstance";
-import {Header, SearchAndFilter} from "../../../components/Global";
-import {CardGrid} from "../../../components/App";
+import React, { useState } from 'react';
+import { GetServerSideProps } from 'next';
+import { getAllPuzzleInstances } from '../../../services/puzzleInstance';
+import { Header, SearchAndFilter } from '../../../components/Global';
+import { CardGrid } from '../../../components/App';
+import styles from '../../../styles/pages/PuzzleList.module.scss';
+import {PuzzleInstance} from '@prisma/client'
 
-// type PuzzleListTypes = {
-//     puzzleInstances: PuzzleInstance[];
-// };
-const PuzzleList = ({puzzleInstances}) => {
-    const [searchNFilter, setSearchNFilter] = useState<{searchText:string, filterFields: {EASY: boolean, MEDIUM: boolean, HARD: boolean}}>({
-        searchText: '',
-        filterFields: {EASY: false, MEDIUM: false, HARD: false}
-    });
-    const searchText = searchNFilter.searchText;
-    const isEasy = searchNFilter.filterFields.EASY;
-    const isMedium = searchNFilter.filterFields.MEDIUM;
-    const isHard = searchNFilter.filterFields.HARD;
+type PuzzleListTypes = {
+    puzzleInstances: PuzzleInstance[]
+}
 
-    return (
-        <>
-            <Header />
-            <SearchAndFilter setSearchNFilterVal={setSearchNFilter}/>
+const PuzzleList = ({puzzleInstances}: PuzzleListTypes  ) => {
+  const [searchNFilter, setSearchNFilter] = useState<{
+    searchText: string;
+    filterFields: { EASY: boolean; MEDIUM: boolean; HARD: boolean };
+  }>({
+    searchText: '',
+    filterFields: { EASY: true, MEDIUM: true, HARD: true }
+  });
+  return (
+    <>
+      <Header />
+      <div className={styles.searchAndFilter}>
+        <SearchAndFilter setSearchNFilterVal={setSearchNFilter} />
+      </div>
 
-            {/*if users did not select filter + seach*/}
-
-            {/*if users did select filter + search}*/}
-            <CardGrid cardList={
-                puzzleInstances
-                    .filter(instance =>
-                        searchNFilter.filterFields[instance.puzzle.difficulty] === true
-                    ).map(instance=>{ //return a card prop object for each instance
-                        return{
-                            name: instance.puzzle.name,
-                            content: instance.puzzle.content,
-                            difficulty: instance.puzzle.difficulty,
-                            buttonActions:[
-                                {
-                                    text: 'View On Map',
-                                    style: 'secondary',
-                                    // action?: () => any, TODO: pass the instanceId to the map page, so user don't to filter again
-                                    link: '/puzzles/map',
-                                },
-                                {
-                                    text: 'Solve Online',
-                                    style: 'primary',
-                                    link: `/puzzles/[${instance.id}]`,
-                                }
-                            ]
-                        };
-                    })
-            }/>
-        </>
-    );
+      <div className={styles.cardGrid}>
+        <CardGrid
+          cardList={puzzleInstances
+            .filter(instance => {
+              if (searchNFilter.searchText) {
+                return (
+                  searchNFilter.filterFields[instance.puzzle.difficulty] ===
+                    true &&
+                  instance.puzzle.name.includes(searchNFilter.searchText)
+                );
+              } else {
+                return (
+                  searchNFilter.filterFields[instance.puzzle.difficulty] ===
+                  true
+                );
+              }
+            })
+            .map(instance => {
+              //return a card prop object for each instance
+              return {
+                name: instance.puzzle.name,
+                content: instance.puzzle.content,
+                difficulty: instance.puzzle.difficulty,
+                buttonActions: [
+                  {
+                    text: 'View On Map',
+                    style: 'secondary',
+                    // action?: () => any, TODO: pass the instanceId to the map page, so user don't to filter again
+                    link: '/puzzles/map'
+                  },
+                  {
+                    text: 'Solve Online',
+                    style: 'primary',
+                    link: `/puzzles/[${instance.id}]`
+                  }
+                ]
+              };
+            })}
+        />
+      </div>
+    </>
+  );
 };
 
 export default PuzzleList;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    const puzzleInstances = await getAllPuzzleInstances(true);
-
-    return {
-        props: {
-            puzzleInstances
-        }
-    };
+  const puzzleInstances = await getAllPuzzleInstances(true);
+  return {
+    props: { puzzleInstances }
+  };
 };
