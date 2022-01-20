508ce87c7c571703d4d70952bf513bfcc86bb2ec
yuqi88 <snow34545@gmail.com>
2022-01-19 19:07:29 -0800

    add test

---
+++ src/__tests__/utils/difficulty.test.ts
@@ -0,0 +1,7 @@
+import {difficultySentenceCase} from "../../utils/difficulty";
+
+it('difficulty in lowercase except for the first letter', ()=>{
+    expect(difficultySentenceCase('EASY')).toBe('Easy');
+    expect(difficultySentenceCase('MEDIUM')).toBe('Medium');
+    expect(difficultySentenceCase('HARD')).toBe('Hard');
+})
