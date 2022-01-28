e312f74fbdbac9dfd7a3b22dbc860fdfe5cdcee7
Kiet Phan <tuankietvn@outlook.com>
2022-01-28 08:11:27 +0700

    Add example api test

--- src/pages/api/__tests__/auth/signup.test.ts
+++ src/pages/api/__tests__/auth/signup.test.ts
@@ -1,35 +1,49 @@
 /**
  * @jest-environment node
  */
-
-import request from 'supertest';
+// Implementation get from https://stackoverflow.com/questions/61640829/how-can-i-test-express-server-with-supertest-in-next-js
 import * as faker from 'faker';
-import { prismaMock as app } from '../singleton';
+import { PrismaClient } from '@prisma/client';
+import signUpHandler from '../../auth/signup';
+import { testClient } from "../../../../utils/test-client";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { checkHash } from '../../../../utils/password';
+
+const request = testClient(signUpHandler);
+const prisma = new PrismaClient();
+
+
+beforeAll(async () => {
+  await prisma.$connect();
+})
+
+afterAll(async () => {
+  await prisma.$disconnect();
+})
 
 describe('Success test', () => {
-  let email: string;
-  let password: string;
+  let email: string = faker.internet.email();
+  let password: string = faker.internet.password();
 
-  beforeEach(async () => {
-    await app.$connect();
-    email = faker.internet.email();
-    password = faker.internet.password();
-  })
-
-  afterEach(async () => {
-    await app.$disconnect();
-  })
-
-
-  it('Create user successs', () => {
+  it('Create user successs', async () => {
     const userData = {
       email: email,
       password: password,
     };
+    await request.post('/api/auth/signup').send(userData).expect(201);
 
-    request(app).post('/api/auth/signup').send(userData).expect(201);
-
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      }
+    })
+    expect(user).toBeDefined();
+    expect(user.email).toBe(email);
+    expect(user.password).not.toBeNull();
+    expect(user.password).not.toBe(password);
+    expect(user.password).toBeDefined();
+    expect(checkHash(password, user.password)).toBe(true);
   })
-})
-
-export { }
+});
