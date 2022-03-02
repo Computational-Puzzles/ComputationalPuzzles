8988a063a0c9e85cf3e9a03820d5700953dec99a
yuqi88 <snow34545@gmail.com>
2022-03-01 16:08:12 -0800

    add random length pw for testing

--- src/__tests__/utils/password.test.ts
+++ src/__tests__/utils/password.test.ts
@@ -6,8 +6,17 @@ describe('testing the hashFunction', () => {
   });
 
   it('should compare the length of 2 hashed passwords', async () => {
-    const hashed1 = await hashFunction('password1');
-    const hashed2 = await hashFunction('pw2');
+    //add 2 random length passwords: 1-10
+    const len1 = Math.floor(Math.random()*10+1);
+    const len2 = Math.floor(Math.random()*10+1);
+    // generate random strings with the length above;
+    const pw1 = getRandomString(len1);
+    const pw2 = getRandomString(len2);
+    console.log("password 1 is " + pw1);
+    console.log("password 2 is " + pw2);
+    //generate 2 hashed pw
+    const hashed1 = await hashFunction(pw1);
+    const hashed2 = await hashFunction(pw2);
     expect(hashed1.length).toBe(hashed2.length);
   });
 });
@@ -16,3 +25,12 @@ describe('testing the hashFunction', () => {
     expect(checkHash('password', await hashFunction('password'))).toBeTruthy();
   });
 });
+
+const getRandomString = (length) =>{
+  const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let str = '';
+  for (let i=0; i<length; i++){
+    str += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return str;
+}
