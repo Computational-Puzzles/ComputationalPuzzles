c3fc801dba773cd347ae40c5a44c35a1081df16c
yuqi88 <snow34545@gmail.com>
2022-03-08 23:03:43 -0800

    add error catching

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,11 +1,19 @@
 import argon2 from 'argon2';
 
 const hashFunction = async (secret: string) => {
-  return argon2.hash(secret, { type: argon2.argon2id });
+  try{
+    return argon2.hash(secret, { type: argon2.argon2id });
+  }catch(err){
+    throw err;
+  }
 };
 
-const checkHash = async (checker: string, hash: string) => {
-  return (await hashFunction(checker)) === hash;
+const checkHash = async (secret: string, hashValue: string) => {
+  try{
+    return await argon2.verify(hashValue, secret);
+  }catch(err){
+    throw err;
+  }
 };
 
 export { hashFunction, checkHash };
