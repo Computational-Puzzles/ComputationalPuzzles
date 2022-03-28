fad712675bfca689600bb0d1da222c8a0acd0a4d
yuqi88 <snow34545@gmail.com>
2022-03-28 16:27:55 -0700

    update using mock puzzles

--- src/__tests__/utils/puzzles.test.ts
+++ src/__tests__/utils/puzzles.test.ts
@@ -1,18 +1,19 @@
 import { checkPuzzleAnswer } from '../../utils/puzzles';
-import puzzle from '../../__mocks__/mock_puzzle';
 import { mockPuzzle } from '../../__mocks__/pages/api/puzzles';
+import { mockRandomSeed } from '../../__mocks__/pages/api/puzzles/instances/submit.ts';
 import { getRandomNumber } from '../../__mocks__/pages/api/getRandom';
 
 it('should compare the user submitted puzzle answer with the ans key in the puzzle (from the db)', () => {
-  // const answer = puzzle.variables?.answer;
+  const puzzle = mockPuzzle();
   const mockAns = mockPuzzle().variables?.answer;
-  if (mockAns) {
+  if (!mockAns) {
     console.log('Answer is not defined.');
   }
   let notAns;
   do {
     notAns = getRandomNumber();
   } while (mockAns === notAns);
-  expect(checkPuzzleAnswer(puzzle, 1, notAns)).toBe(false);
-  expect(checkPuzzleAnswer(puzzle, 1, mockAns)).toBe(true);
+  const mockSeed = mockRandomSeed();
+  expect(checkPuzzleAnswer(puzzle, mockSeed, notAns)).toBe(false);
+  expect(checkPuzzleAnswer(puzzle, mockSeed , mockAns)).toBe(true);
 });
