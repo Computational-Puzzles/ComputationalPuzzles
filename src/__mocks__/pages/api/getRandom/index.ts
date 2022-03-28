bab32e0a13faf3549d729c36960f29e149964913
yuqi88 <snow34545@gmail.com>
2022-03-28 16:11:30 -0700

    add functions using faker

---
+++ src/__mocks__/pages/api/getRandom/index.ts
@@ -0,0 +1,9 @@
+import * as faker from 'faker';
+
+export const getRandomString = (length) =>{
+    return faker.datatype.string(length);
+};
+
+export const getRandomNumber = () =>{
+    return faker.datatype.number()
+};
