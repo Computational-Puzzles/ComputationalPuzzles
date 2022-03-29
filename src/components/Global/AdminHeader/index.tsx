9b9355a7decf77a3eb5de949ffc8900333c4da81
yuqi88 <snow34545@gmail.com>
2022-03-29 11:54:13 -0700

    add admin header

---
+++ src/components/Global/AdminHeader/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './AdminHeader.module.scss'
+
+const AdminHeader = () => {
+    const currentPage = 'Admin Dashboard';
+    return (
+        <div>
+            <div className={styles.pageName}>
+                <p>{currentPage}</p>
+            </div>
+        </div>
+    );
+};
+
+export default AdminHeader;
