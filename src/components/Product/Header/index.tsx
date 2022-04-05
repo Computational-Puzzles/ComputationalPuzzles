61a6b991ad9a417eadd2ff9453b8c967fb5c1c1e
yuqi88 <snow34545@gmail.com>
2022-04-04 19:04:29 -0700

    add sign-in button in header

--- src/components/Product/Header/index.tsx
+++ src/components/Product/Header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSession } from 'next-auth/react';
-import { Button, Logo, LogStatusButton } from '../../Global';
+import {Button, Logo, LogStatusButton, SignStatusButton} from '../../Global';
 import headerStyles from './Header.module.scss';
 import productStyles from '../../../styles/pages/Product.module.scss';
 
@@ -13,6 +13,7 @@ const Header = () => {
         <div className={`${productStyles.wrapper} ${headerStyles.content}`}>
           <Logo showMark={true} showType={true} link={true} />
           <div className={headerStyles.actions}>
+            <SignStatusButton status ={status}/>
             <LogStatusButton status={status} />
             <Button
               style={'outline'}
