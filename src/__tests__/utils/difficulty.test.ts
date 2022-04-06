7369f5cb048b64c411a6d0e6afe92a4fb66b741d
yuqi88 <snow34545@gmail.com>
2022-04-06 06:21:22 -0700

    add test description

--- src/__tests__/utils/difficulty.test.ts
+++ src/__tests__/utils/difficulty.test.ts
@@ -1,7 +1,14 @@
 import { difficultySentenceCase } from '../../utils/difficulty';
 
-it('difficulty in lowercase except for the first letter', () => {
-  expect(difficultySentenceCase('EASY')).toBe('Easy');
-  expect(difficultySentenceCase('MEDIUM')).toBe('Medium');
-  expect(difficultySentenceCase('HARD')).toBe('Hard');
+describe('Difficulties are in lowercase except for the first letter.', ()=>{
+  it('Difficulty = EASY correct ', () => {
+    expect(difficultySentenceCase('EASY')).toBe('Easy');
+  });
+  it('Difficulty = MEDIUM correct ', () => {
+    expect(difficultySentenceCase('MEDIUM')).toBe('Medium');
+  });
+  it('Difficulty = HARD correct ', () => {
+    expect(difficultySentenceCase('HARD')).toBe('Hard');
+  });
 });
+
