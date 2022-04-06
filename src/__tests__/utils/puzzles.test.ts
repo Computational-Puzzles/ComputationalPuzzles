832ccb7bfbe88896e685ec063ff980888b12531f
yuqi88 <snow34545@gmail.com>
2022-04-06 08:14:23 -0700

    update test

--- src/__tests__/utils/puzzles.test.ts
+++ src/__tests__/utils/puzzles.test.ts
@@ -1,13 +1,27 @@
 import { checkPuzzleAnswer } from '../../utils/puzzles';
-import {mockMCQPuzzle, mockTxtPuzzle} from '../../__mocks__/pages/api/puzzles';
+import { mockPuzzle } from '../../__mocks__/pages/api/puzzles';
 import { mockRandomSeed } from '../../__mocks__/pages/api/puzzles/instances/submit';
 import {getRandomString} from '../../__mocks__/getRandom';
+import {prisma} from "../../__mocks__";
+import {mockOfficialAnswer, mockOptionsNoAns} from "../../__mocks__/pages/api/puzzles/instances/create";
 
 describe('testing checkPuzzleAnswer: Comparing user submitted answer with the ans key in the puzzle (from the db) - ', () => {
     it('part1: MCQ', async () => {
         const mockSeed = mockRandomSeed();
-        const pzl =  await mockMCQPuzzle();
-        const ans = pzl.variables['answer'];
+        const ans = mockOfficialAnswer();
+        const pzl = await mockPuzzle();
+        await prisma.puzzle.update({
+            where: {
+                id: pzl.id
+            },
+            data: {
+                inputType: 'MCQ',
+                variables: {
+                    answer: {ans},
+                    options: mockOptionsNoAns().push(ans)
+                },
+            }
+        });
         let notAns = getRandomString();
         while(ans === notAns){
             notAns = getRandomString();
@@ -18,7 +32,18 @@ describe('testing checkPuzzleAnswer: Comparing user submitted answer with the an
 
     it('part2: Text question', async () => {
         const mockSeed = mockRandomSeed();
-        const pzl =  await mockTxtPuzzle();
+        const pzl = await mockPuzzle();
+        await prisma.puzzle.update({
+            where: {
+                id: pzl.id
+            },
+            data: {
+                variables: {
+                    answer: mockOfficialAnswer(),
+                    options: []
+                },
+            }
+        });
         const ans = pzl.variables['answer'];
         let notAns = getRandomString();
         while(ans === notAns){
--- src/utils/puzzles.ts
+++ src/utils/puzzles.ts
@@ -6,6 +6,7 @@ const checkPuzzleAnswer = (
   puzzleAnswer: string
 ) => {
   const trueAnswer = puzzle.variables['answer'];
+  console.log("in the checkPuzzleAnswer function, the accessed ans = " + trueAnswer);
   if (!trueAnswer) return false;
   return trueAnswer === puzzleAnswer;
 };
