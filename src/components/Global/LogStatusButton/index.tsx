11b67c7495feac7c8459156ce38ef9012af5f46a
yuqi88 <snow34545@gmail.com>
2022-04-13 00:13:48 -0700

    remove duplicates

--- src/components/Global/LogStatusButton/index.tsx
+++ src/components/Global/LogStatusButton/index.tsx
@@ -4,19 +4,13 @@ import React from 'react';
 import { LogStatusButtonProps } from '../../../types/logStatusButton';
 
 const LogStatusButton = ({ status, useLogIn }: LogStatusButtonProps) => {
-  if (useLogIn) {
-    return status === 'authenticated' ? (
-      <Button style={'flat'} content={'Logout'} onClick={() => signOut()} />
-    ) : (
-      <Button style={'flat'} content={'Login'} onClick={() => signIn()} />
-    );
-  } else {
-    return status === 'authenticated' ? (
-      <Button style={'flat'} content={'Logout'} onClick={() => signOut()} />
-    ) : (
-      <Button style={'flat'} content={'Sign In'} link={'/auth/signup'} />
-    );
-  }
+  return status === 'authenticated' ? (
+    <Button style={'flat'} content={'Logout'} onClick={() => signOut()} />
+  ) : useLogIn ? (
+    <Button style={'flat'} content={'Login'} onClick={() => signIn()} />
+  ) : (
+    <Button style={'flat'} content={'Sign In'} link={'/auth/signup'} />
+  );
 };
 
 export default LogStatusButton;
