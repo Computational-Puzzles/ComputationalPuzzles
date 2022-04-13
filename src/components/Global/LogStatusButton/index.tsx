b1bd5dfd8580cb7f6ebf022a9bde6e830d94dabb
yuqi88 <snow34545@gmail.com>
2022-04-12 23:08:23 -0700

    run prettify

--- src/components/Global/LogStatusButton/index.tsx
+++ src/components/Global/LogStatusButton/index.tsx
@@ -6,15 +6,15 @@ import { LogStatusButtonProps } from '../../../types/logStatusButton';
 const LogStatusButton = ({ status, useLogIn }: LogStatusButtonProps) => {
   if (useLogIn) {
     return status === 'authenticated' ? (
-        <Button style={'flat'} content={'Logout'} onClick={() => signOut()} />
+      <Button style={'flat'} content={'Logout'} onClick={() => signOut()} />
     ) : (
-        <Button style={'flat'} content={'Login'} onClick={() => signIn()} />
+      <Button style={'flat'} content={'Login'} onClick={() => signIn()} />
     );
   } else {
     return status === 'authenticated' ? (
-        <Button style={'flat'} content={'Logout'} onClick={() => signOut()} />
+      <Button style={'flat'} content={'Logout'} onClick={() => signOut()} />
     ) : (
-        <Button style={'flat'} content={'Sign In'} link={'/auth/signup'} />
+      <Button style={'flat'} content={'Sign In'} link={'/auth/signup'} />
     );
   }
 };
