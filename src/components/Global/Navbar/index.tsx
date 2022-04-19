1b1ea9d105fb8aed096ef464e6089d1540f17a01
Mathew de Vin <mathewdevin03@gmail.com>
2022-04-19 16:57:16 -0700

    Fix conflicts

--- src/components/Global/Navbar/index.tsx
+++ src/components/Global/Navbar/index.tsx
@@ -1,30 +1,30 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import styles from './Navbar.module.scss';
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { Button, Logo, Sidebar, LogStatusButton } from '..';
-import { HeaderProps } from '../../../types/global';
-import { isAdmin } from '../../../services';
-import { useSession } from 'next-auth/react';
+import {useRouter} from 'next/router';
+import {Button, Logo, Sidebar, LogStatusButton} from '..';
+import {HeaderProps} from '../../../types/global';
+import {isAdmin} from '../../../services';
+import {useSession} from 'next-auth/react';
 
-const Header = ({ profilePicture }: HeaderProps) => {
+const Header = ({profilePicture}: HeaderProps) => {
   const [activeTab, setActiveTab] = useState<0 | 1>(0);
   const router = useRouter();
   const [validAdmin, setValidAdmin] = useState(false);
-  const { data: session, status } = useSession();
+  const {data: session, status} = useSession();
   const email = session?.user?.email;
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
-
+  
   useEffect(() => {
-    isAdmin({ email }).then(res => setValidAdmin(res));
+    isAdmin({email}).then(res => setValidAdmin(res));
   }, [email]);
-
+  
   const openMenu = () => {
     setMenuOpen(true);
   };
-
+  
   const NavContent = useCallback(
     (displayMenu: boolean) => {
       return (
@@ -53,37 +53,37 @@ const Header = ({ profilePicture }: HeaderProps) => {
           </nav>
           {status !== 'authenticated' ? (
             <div className={styles.right}>
-              <LogStatusButton status={status} />
+              <LogStatusButton status={status} useLogIn/>
             </div>
-            ) : (
-            {validAdmin && (
-              <Button style={'flat'} content={'Admin'} link={'/admin'} />
-            )}
-            <Button
-              style={'outline'}
-              content={'Profile'}
-              link={'/auth/profile'}
-            />
-            {!displayMenu &&
-              (profilePicture ? (
-                <Image
-                  className={styles.profileImg}
-                  src={profilePicture}
-                  alt="profile"
-                />
-              ) : (
-                <span className={styles.profileImg}> </span>
-              ))}
+          ) : (
+            <>
+              {validAdmin && <Button style={'flat'} content={'Admin'} link={'/admin'}/>}
+              <Button
+                style={'outline'}
+                content={'Profile'}
+                link={'/auth/profile'}
+              />
+              {!displayMenu &&
+                (profilePicture ? (
+                  <Image
+                    className={styles.profileImg}
+                    src={profilePicture}
+                    alt="profile"
+                  />
+                ) : (
+                  <span className={styles.profileImg}> </span>
+                ))}
+            </>
           )}
         </>
       );
     },
-    [validAdmin, router.asPath, profilePicture]
+    [router.asPath, status, validAdmin, profilePicture]
   );
-
+  
   return (
     <div className={styles.header}>
-      <Logo showMark={true} showType={true} link={true} />
+      <Logo showMark={true} showType={true} link={true}/>
       <div className={styles.header_content}>{NavContent(false)}</div>
       <button className={styles.header_menu__button} onClick={openMenu}>
         <svg
@@ -102,7 +102,7 @@ const Header = ({ profilePicture }: HeaderProps) => {
         </svg>
       </button>
       <Sidebar sidebarOpen={menuOpen} setSidebarOpen={setMenuOpen}>
-        <Logo showMark={true} showType={false} link={true} />
+        <Logo showMark={true} showType={false} link={true}/>
         {NavContent(true)}
       </Sidebar>
     </div>
